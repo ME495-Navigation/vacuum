ad95ab61170ef532203efb2a9699cfb78e4d9618
Matthew Elwin <elwin@northwestern.edu>
2021-01-19 00:28:21 -0600

    Basic service advertised and working.

--- src/vacuum.cpp
+++ src/vacuum.cpp
@@ -5,9 +5,33 @@
 #include"ros/ros.h"
 #include"std_srvs/Empty.h"
 
+class Vacuum
+{
+    public:
+        Vacuum():
+            nh(),
+            start(nh.advertiseService("start", &Vacuum::start_callback, this))
+        {
+            ROS_INFO_STREAM("HELLO");
+        }
+
+        bool start_callback(std_srvs::Empty::Request &, std_srvs::Empty::Response &) 
+        {
+            ROS_INFO_STREAM("Starting!");
+            return true;
+        }
+
+    private:
+        ros::NodeHandle nh;
+        ros::ServiceServer start;
+};
 
 int main(int argc, char * argv[])
 {
     ros::init(argc, argv, "vacuum");
+
+    ros::NodeHandle nh;
+    const Vacuum v;
+    ros::spin();
     return 0;
 }
