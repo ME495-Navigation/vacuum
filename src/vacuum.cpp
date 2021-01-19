0dc4d61edc4fb92f1d833f5a2369fb49688fdf2f
Matthew Elwin <elwin@northwestern.edu>
2021-01-19 00:37:27 -0600

    Added the basic state machine.

--- src/vacuum.cpp
+++ src/vacuum.cpp
@@ -10,27 +10,44 @@ class Vacuum
     public:
         Vacuum():
             nh(),
-            start(nh.advertiseService("start", &Vacuum::start_callback, this))
+            start(nh.advertiseService("start", &Vacuum::start_callback, this)),
+            loop(nh.createTimer(ros::Duration(1.0), &Vacuum::timer_callback, this)),
+            state(State::CHARGING)
         {
-            ROS_INFO_STREAM("HELLO");
         }
 
         bool start_callback(std_srvs::Empty::Request &, std_srvs::Empty::Response &) 
         {
-            ROS_INFO_STREAM("Starting!");
             return true;
         }
 
+        void timer_callback(const ros::TimerEvent &)
+        {
+            switch(state)
+            {
+                case State::CHARGING:
+                    ROS_INFO("Charging");
+                    break;
+                case State::VACUUMING:
+                    ROS_INFO("Vacuuming");
+                    break;
+                case State::RETURNING:
+                    ROS_INFO("Returning");
+                    break;
+                default:
+                    throw std::logic_error("Invalid State");
+            }
+        }
     private:
         ros::NodeHandle nh;
         ros::ServiceServer start;
+        ros::Timer loop;
+        enum class State {CHARGING, VACUUMING, RETURNING} state;
 };
 
 int main(int argc, char * argv[])
 {
     ros::init(argc, argv, "vacuum");
-
-    ros::NodeHandle nh;
     const Vacuum v;
     ros::spin();
     return 0;
