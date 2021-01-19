81f37440df7362da9da36e22667f14612f57acb2
Matthew Elwin <elwin@northwestern.edu>
2021-01-19 00:02:10 -0600

    Updated comments at top of file.

--- src/vacuum.cpp
+++ src/vacuum.cpp
@@ -1,5 +1,10 @@
+/// \brief A node that simulates a robot vacuum cleaner
+/// 
+/// Services:
+///  start - (std_srvs/Empty) start the vacuum cleaner if it is not charging
 #include"ros/ros.h"
 #include"std_srvs/Empty.h"
+
 
 int main(int argc, char * argv[])
 {
